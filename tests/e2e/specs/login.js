1df017049c3caaad2e46e0a3192a629763b8c2b7
Boaibai <278803847@qq.com>
2023-06-16 09:28:50 +0800

    登录正常、失败用例

--- tests/e2e/specs/login.js
+++ tests/e2e/specs/login.js
@@ -1,82 +1,146 @@
 // https://docs.cypress.io/api/table-of-contents
 
-describe('登录注册子系统-用户登录正常', () => {
-
-  it('求职者登录-ID', () => {
-    //访问网站
+describe('登录注册子系统-用户登录', () => {
+  it('用户登录正常', () => {
+    //求职者登录-ID
     cy.visit('/')
-    //输入账号密码
+    //访问网站
     cy.get(':nth-child(1) > .el-form-item__content > .el-input > .el-input__wrapper').type("10014")
     cy.get(':nth-child(2) > .el-form-item__content > .el-input > .el-input__wrapper').type("021121")
-    //点击登录按钮
+    //输入账号密码
     cy.get("#login").click()
-    //断言
+    //点击登录按钮
     cy.get('#message_1').should('contain', '登录成功');
     cy.url().should('contain','jobhunter')
-  })
+    //断言
 
-  it('求职者登录-邮箱', () => {
-    //访问网站
+    //求职者登录-邮箱
     cy.visit('/')
-    //输入账号密码
+    //访问网站
     cy.get(':nth-child(1) > .el-form-item__content > .el-input > .el-input__wrapper').type("278803847@qq.com")
     cy.get(':nth-child(2) > .el-form-item__content > .el-input > .el-input__wrapper').type("021121")
-    //点击登录按钮
+    //输入账号密码
     cy.get("#login").click()
-    //断言
+    //点击登录按钮
     cy.get('#message_1').should('contain', '登录成功');
     cy.url().should('contain','jobhunter')
-  })
-  
-  it('招聘方登录-ID', () => {
-    //访问网站
+    //断言
+
+    //招聘方登录-ID
     cy.visit('/')
-    //输入账号密码
+    //访问网站
     cy.get(':nth-child(1) > .el-form-item__content > .el-input > .el-input__wrapper').type("10018")
     cy.get(':nth-child(2) > .el-form-item__content > .el-input > .el-input__wrapper').type("021121")
-    //点击登录按钮
+    //输入账号密码
     cy.get("#login").click()
-    //断言
+    //点击登录按钮
     cy.get('#message_1').should('contain', '登录成功');
     cy.url().should('contain','recruiter')
-  })
+    //断言
 
-  it('招聘方登录-邮箱', () => {
-    //访问网站
+    //招聘方登录-邮箱
     cy.visit('/')
-    //输入账号密码
+    //访问网站
     cy.get(':nth-child(1) > .el-form-item__content > .el-input > .el-input__wrapper').type("123@qq.com")
     cy.get(':nth-child(2) > .el-form-item__content > .el-input > .el-input__wrapper').type("021121")
-    //点击登录按钮
+    //输入账号密码
     cy.get("#login").click()
-    //断言
+    //点击登录按钮  
     cy.get('#message_1').should('contain', '登录成功');
     cy.url().should('contain','recruiter')
-  })
+    //断言
 
-  it('管理员登录-ID', () => {
-    //访问网站
+    //管理员登录-ID
     cy.visit('/')
-    //输入账号密码
+    //访问网站
     cy.get(':nth-child(1) > .el-form-item__content > .el-input > .el-input__wrapper').type("10007")
     cy.get(':nth-child(2) > .el-form-item__content > .el-input > .el-input__wrapper').type("admin")
-    //点击登录按钮
+    //输入账号密码
     cy.get("#login").click()
-    //断言
+    //点击登录按钮
     cy.get('#message_1').should('contain', '登录成功');
     cy.url().should('contain','admin')
-  })
+    //断言
 
-  it('管理员登录-邮箱', () => {
-    //访问网站
+    //管理员登录-邮箱
     cy.visit('/')
-    //输入账号密码
+    //访问网站
     cy.get(':nth-child(1) > .el-form-item__content > .el-input > .el-input__wrapper').type("admin@qq.com")
     cy.get(':nth-child(2) > .el-form-item__content > .el-input > .el-input__wrapper').type("admin")
-    //点击登录按钮
+    //输入账号密码
     cy.get("#login").click()
-    //断言
+    //点击登录按钮
     cy.get('#message_1').should('contain', '登录成功');
     cy.url().should('contain','admin')
+    //断言
   })
+  it('登录注册子系统-用户登录异常', () => {
+    //求职者登录-账号不存在
+    
+    cy.visit('/')
+    //访问网站
+    cy.get(':nth-child(1) > .el-form-item__content > .el-input > .el-input__wrapper').type("11014")
+    cy.get(':nth-child(2) > .el-form-item__content > .el-input > .el-input__wrapper').type("021121")
+    //输入账号密码
+    cy.get("#login").click()
+    //点击登录按钮
+    cy.get('#message_1').should('contain', '登录失败');
+    //断言
+
+    //求职者登录-密码错误
+    cy.visit('/')
+    //访问网站
+    cy.get(':nth-child(1) > .el-form-item__content > .el-input > .el-input__wrapper').type("10014")
+    cy.get(':nth-child(2) > .el-form-item__content > .el-input > .el-input__wrapper').type("021111")
+    //输入账号密码
+    cy.get("#login").click()
+    //点击登录按钮
+    cy.get('#message_1').should('contain', '登录失败');
+    //断言
+
+    //招聘方登录-账号不存在
+    cy.visit('/')
+    //访问网站
+    cy.get(':nth-child(1) > .el-form-item__content > .el-input > .el-input__wrapper').type("11018")
+    cy.get(':nth-child(2) > .el-form-item__content > .el-input > .el-input__wrapper').type("021121")
+    //输入账号密码
+    cy.get("#login").click()
+    //点击登录按钮
+    cy.get('#message_1').should('contain', '登录失败');
+    //断言
+
+    //招聘方登录-密码错误
+    cy.visit('/')
+    //访问网站
+    cy.get(':nth-child(1) > .el-form-item__content > .el-input > .el-input__wrapper').type("10018")
+    cy.get(':nth-child(2) > .el-form-item__content > .el-input > .el-input__wrapper').type("021111")
+    //输入账号密码
+    cy.get("#login").click()
+    //点击登录按钮
+    cy.get('#message_1').should('contain', '登录失败');
+    //断言
+
+    //管理员登录-账号不存在
+    cy.visit('/')
+    //访问网站
+    cy.get(':nth-child(1) > .el-form-item__content > .el-input > .el-input__wrapper').type("11007")
+    cy.get(':nth-child(2) > .el-form-item__content > .el-input > .el-input__wrapper').type("admin")
+    //输入账号密码
+    cy.get("#login").click()
+    //点击登录按钮
+    cy.get('#message_1').should('contain', '登录失败');
+    //断言
+
+    //管理员登录-密码错误
+    cy.visit('/')
+    //访问网站
+    cy.get(':nth-child(1) > .el-form-item__content > .el-input > .el-input__wrapper').type("10007")
+    cy.get(':nth-child(2) > .el-form-item__content > .el-input > .el-input__wrapper').type("admin1")
+    //输入账号密码
+    cy.get("#login").click()
+    //点击登录按钮
+    cy.get('#message_1').should('contain', '登录失败');
+    //断言
+  })
+
 })
