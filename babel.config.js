15b95dd274fa6fcbb567ef381505f061b4720284
Boaibai <278803847@qq.com>
2023-06-21 01:00:50 +0800

    配置覆盖率插件

--- babel.config.js
+++ babel.config.js
@@ -3,3 +3,9 @@ module.exports = {
     '@vue/cli-plugin-babel/preset'
   ]
 }
+// Only instrument code when running e2e tests
+if (process.env.npm_lifecycle_script === 'vue-cli-service test:e2e') {console.log('Instrument the code')
+  module.exports.plugins = [
+    ['istanbul', {useInlineSourceMaps: false}]
+  ]
+}
