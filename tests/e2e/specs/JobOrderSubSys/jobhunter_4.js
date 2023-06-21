429eb41de99ee9d728741107fa4cc9d1266e95c1
Boaibai <278803847@qq.com>
2023-06-21 18:10:32 +0800

    脚本压缩

--- tests/e2e/specs/JobOrderSubSys/jobhunter_4.js
+++ tests/e2e/specs/JobOrderSubSys/jobhunter_4.js
@@ -30,25 +30,6 @@ describe('兼职订单子系统-求职者', () => {
         //断言
 
         //用户未提交申诉，或者提交的申诉状态为未通过
-        cy.visit('/')
-        //访问网站
-        cy.get(':nth-child(1) > .el-form-item__content > .el-input > .el-input__wrapper').type("10014")
-        cy.get(':nth-child(2) > .el-form-item__content > .el-input > .el-input__wrapper').type("021121")
-        //输入账号密码
-        cy.get("#login").click()
-        //点击登录按钮
-        cy.get('.nav > [href="/jobhunter/person"]').click()
-        //跳转至个人中心-我的信息页面
-        cy.get('.el-menu > :nth-child(4)').click()
-        //跳转到兼职订单页
-        cy.wait(200)
-        cy.get('#tab-已完成').click()
-        //筛选已完成的订单
-        cy.wait(2000)
-        cy.get('button.el-button').filter(':contains("申诉")').filter(':visible').first().click();      
-        //点击申诉按钮
-        cy.get(':nth-child(2) > .el-radio__input > .el-radio__inner').click()
-        //选择类型为支付申诉
         cy.get('.el-textarea__inner').clear()
         cy.get('.el-textarea__inner').type("未支付")
         cy.get('.dialog-footer > :nth-child(2)').click()
--- tests/e2e/specs/JobOrderSubSys/recruiter_3.js
+++ tests/e2e/specs/JobOrderSubSys/recruiter_3.js
@@ -37,33 +37,6 @@ describe('兼职订单子系统-招聘方', () => {
         //断言
 
         //用户未提交申诉，或者提交的申诉状态为未通过
-        cy.visit('/')
-        //访问网站
-        cy.get(':nth-child(1) > .el-form-item__content > .el-input > .el-input__wrapper').type("10018")
-        cy.get(':nth-child(2) > .el-form-item__content > .el-input > .el-input__wrapper').type("021121")
-        //输入账号密码
-        cy.get("#login").click()
-        //点击登录按钮
-        cy.get('[href="/recruiter/jobManage"]').click()
-        //跳转至兼职管理界面
-        cy.wait(200)
-        cy.get('button.el-button').filter(':contains("已通过")').filter(':visible').first().click();   
-        cy.wait(2000)
-        cy.get('button.el-button').filter(':contains("已通过")').filter(':visible').first().click();   
-        //点击筛选“已通过”的兼职
-        cy.wait(2000)
-        cy.get('.el-card > .el-card__body').contains('.work_name', '大学生线上讲解小学数学长期兼职')
-        .parents('.el-card > .el-card__body')
-        .within(() => {
-            // Click the "查看详情" button
-            cy.contains('.el-button', '查看详情').click();
-        });
-        //跳转至兼职“大学生线上讲解小学数学长期兼职”详情页
-        cy.get(':nth-child(3) > :nth-child(7)').scrollIntoView()
-        cy.wait(1000)
-        //查看求职者已完成列表
-        cy.get('button.el-button').filter(':contains("申诉")').filter(':visible').first().click();
-        //点击申诉
         cy.get('.el-textarea__inner').clear()
         cy.get('.el-textarea__inner').type("打分不合理")
         cy.get('.dialog-footer > .el-button--primary').click()
